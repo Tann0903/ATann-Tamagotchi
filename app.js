2d079edc6040c7176a00a37192850c9f47bc0e88
aktann615 <ambertann615@gmail.com>
2024-04-24 22:48:29 -0400

    second project commit

---
+++ app.js
@@ -0,0 +1,10 @@
+class Tamagotchi {
+    constructor (name, age) {
+        this.name = name;
+        this.age = 0;
+    }
+}
+
+// Pet is going to be either continously walking but standing idle until something their name is selected and then when they are bored
+
+// 
