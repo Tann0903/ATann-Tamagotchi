0be3cc4f3937f263bcc2e119de255825343efd77
aktann615 <ambertann615@gmail.com>
2024-04-27 22:22:50 -0400

    third project commit

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-class Tamagotchi {
-    constructor (name, age) {
-        this.name = name;
-        this.age = 0;
-    }
-}
+// class Tamagotchi {
+//     constructor (name, age) {
+//         this.name = name;
+//         this.age = 0;
+//     }
+// }
 
 // Pet is going to be either continously walking but standing idle until something their name is selected and then when they are bored
 
-// 
+//
+
+let sleepButton = document.querySelector("#sleepButton");
+
+sleepButton.addEventListener("click", () => {
+  // why are we using an arrow function here?
+  document.body.style.backgroundImage =
+    "url('https://img.freepik.com/free-vector/pet-cemetery-night-animal-graveyard-with-tombstones-grave-tombs-with-cats-dogs-birds-souls-near-monuments-with-their-photos-full-moon-dark-starry-sky-cartoon-illustration_107791-5902.jpg')";
+    document.querySelector('h1').style.color='white';
+});
